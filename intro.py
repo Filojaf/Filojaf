ae156083fcc0368eb57a94186e7b7e4d22218412
Filojaf <110182038+Filojaf@users.noreply.github.com>
2022-09-02 23:04:13 +0100

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,50 @@
+# this is a comment
+
+# comments are important to explain your code , add titles etc.
+
+# when the python file is run, the comment lines will not run
+
+# pthon syntax
+
+# using keywords, indentation and variables
+
+# examples of keywords
+"""
+def
+class
+print
+input
+"""
+
+# to output to your users
+print(5)
+
+# the above print statement will output 5 to the screen
+
+# Output your name to the screen
+print("Jafar")
+
+# variables
+
+# use variables to hold text, numbers, fnctions etc.
+
+name = "James"
+
+age = 25
+
+print(name, age)
+
+print(f"{name} is {age} years old ")
+
+
+# get user input
+
+input("Enter your name")
+
+# use the name received from the user
+
+print("Welcome")
+
+#solution
+user = input("Enter your name:")
+print("Welcome")
